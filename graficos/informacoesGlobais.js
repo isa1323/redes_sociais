910d55d904c1f37d9e17465ea282691d55cd7c31
isabella <105920830+isa1323@users.noreply.github.com>
2024-10-25 11:47:37 +0000

    ;

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,7 +2,7 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
-    const dados = await res.jons();
+    const dados = await res.json();
     console.log(dados);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
